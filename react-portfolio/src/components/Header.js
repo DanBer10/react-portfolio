1bb95ad5c6a7b6c6689dd2d25711bdfd27436258
DanBer10 <dannebernhardt@outlook.com>
2021-08-11 12:36:07 +0200

    ..

--- react-portfolio/src/components/Header.js
+++ react-portfolio/src/components/Header.js
@@ -13,7 +13,7 @@ import logo from "../images/bernhardt-trans.png";
 const Header = () => {
   return (
     <>
-      <Router basename="gh-pages/">
+      <Router basename="react-portfolio">
         <header>
           <div className="inner-header content-standard">
             <div className="logo-header">
